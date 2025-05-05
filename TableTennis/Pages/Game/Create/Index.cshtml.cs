53146ef65b45acad0df44fa3fa30297276f301ad
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-05-05 15:47:37 +0200

    added create and readall pages to homepage

---
+++ TableTennis/Pages/Game/Create/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TableTennis.Pages.Game.Create
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TableTennis/Pages/Game/ReadAll/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TableTennis.Pages.Game.ReadAll
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
