e6288fa529516ca2a115946693a23f0ef0e46ec3
r9bin <robinlinerudh@hotmail.com>
2025-05-12 12:39:20 +0200

    Adding Set Entity DTO

---
+++ DataAccessLayer/Data/DTO/Sets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data.DTO
+{
+    public class Sets
+    {
+        public int Id { get; set; }
+        public string Player1UserName { get; set; }
+        public string Player2UserName { get; set; }
+        public int Player1Age { get; set; }
+        public int Player2Age { get; set; }
+        public string SetGender { get; set; }
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+        public int IsPlayer1Servce { get; set; }
+        public int ServeCounter { get; set; }
+        public DateOnly MatchDate { get; set; }
+        public string WinnerPlayer { get; set; }
+
+    }
+}
