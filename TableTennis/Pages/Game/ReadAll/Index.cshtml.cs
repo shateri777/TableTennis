83f6b5e89bb4c269851da65b3b0ebf11748952ff
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-05-13 19:56:17 +0200

    added login and logout to nav bar, added styling to it, made history page only show for admins

--- TableTennis/Pages/Game/ReadAll/Index.cshtml.cs
+++ TableTennis/Pages/Game/ReadAll/Index.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TableTennis.Pages.Game.ReadAll
 {
+    [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
         public void OnGet()
