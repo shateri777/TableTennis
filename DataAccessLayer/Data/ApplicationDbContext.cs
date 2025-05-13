d50045b812f2ec4271e050dc2f784b5ff9818782
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-05-13 19:02:47 +0200

    removed double appdbcontext, added seed for role and users in identity

--- DataAccessLayer/Data/ApplicationDbContext.cs
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using DataAccessLayer.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -12,6 +13,12 @@ namespace DataAccessLayer.Data
         }
         public DbSet<TableTennisSet> Sets { get; set; }
         public DbSet<TableTennisMatch> Match{ get; set; }
-
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
     }
 }
--- DataAccessLayer/Data/DataInitializer.cs
+++ DataAccessLayer/Data/DataInitializer.cs
@@ -22,8 +22,39 @@ namespace DataAccessLayer.Data
         public void SeedData()
         {
             _dbContext.Database.Migrate();
-            //SeedRoles();
-            //SeedUsers();
+            SeedRoles();
+            SeedUsers();
+        }
+        private void SeedUsers()
+        {
+            AddUserIfNotExists("admin@angby.se", "*Admin100", new string[] { "Admin" });
+        }
+        private void SeedRoles()
+        {
+            AddRoleIfNotExisting("Admin");
+        }
+        private void AddRoleIfNotExisting(string roleName)
+        {
+            var role = _dbContext.Roles.FirstOrDefault(r => r.Name == roleName);
+            if (role == null)
+            {
+                _dbContext.Roles.Add(new IdentityRole { Name = roleName, NormalizedName = roleName });
+                _dbContext.SaveChanges();
+            }
+        }
+
+        private void AddUserIfNotExists(string userName, string password, string[] roles)
+        {
+            if (_userManager.FindByEmailAsync(userName).Result != null) return;
+
+            var user = new IdentityUser
+            {
+                UserName = userName,
+                Email = userName,
+                EmailConfirmed = true   
+            };
+            _userManager.CreateAsync(user, password).Wait();
+            _userManager.AddToRolesAsync(user, roles).Wait();
         }
     }
 }
