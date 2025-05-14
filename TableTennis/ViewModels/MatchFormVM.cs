40ea98f26e4e422a08b5aed90ccdd8c39dacf316
arvid404 <h21arsod@du.se>
2025-05-14 20:03:39 +0200

    Styling på MatchForm
    Styling på  MatchForm

--- TableTennis/ViewModels/MatchFormVM.cs
+++ TableTennis/ViewModels/MatchFormVM.cs
@@ -4,27 +4,27 @@ namespace TableTennis.ViewModels
 {
     public class MatchFormVM
     {
-        [Required]
+        [Required(ErrorMessage = "Förnamn krävs")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Namnet måste vara minst 2 bokstäver, max 50.")]
         public string Player1FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Efternamn krävs")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Namnet måste vara minst 2 bokstäver, max 50.")]
         public string Player1LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Förnamn krävs")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Namnet måste vara minst 2 bokstäver, max 50.")]
         public string Player2FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Efternamn krävs")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Namnet måste vara minst 2 bokstäver, max 50.")]
         public string Player2LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ålder krävs")]
         [Range(1, 100, ErrorMessage = "Åldern måste vara mellan 1 och 100.")]
-        public int Player1Age { get; set; }
+        public int Player1Age { get; set; }  //"int?" is needed for correct error message here. dont know how to make it work. 
 
-        [Required]
+        [Required(ErrorMessage = "Ålder krävs")]
         [Range(1, 100, ErrorMessage = "Åldern måste vara mellan 1 och 100.")]
         public int Player2Age { get; set; }
     }
