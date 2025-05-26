abf208b24dfdd7a29588f4465e2b0c7dd54b42df
r9bin <robinlinerudh@hotmail.com>
2025-05-26 12:33:27 +0200

    UI fixes to make sure we dont have to scroll in match stuff
    added Ledartavlan and Spelarstatistik in Header

---
+++ TableTennis/Pages/Game/Leaderboard/LeaderboardPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TableTennis.Pages.Game.Leaderboard
+{
+    public class LeaderboardPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TableTennis/Pages/Game/Statistics/StatisticsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TableTennis.Pages.Game.Statistics
+{
+    public class statisticsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
