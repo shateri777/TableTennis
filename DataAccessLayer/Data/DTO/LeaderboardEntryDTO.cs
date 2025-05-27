9f413ebc90d9082c31d90a84f8f51be380e15bd3
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2025-05-27 22:51:20 +0200

    added LeaderboardEntryDTO and LeaderboardEntryVM.

---
+++ DataAccessLayer/Data/DTO/LeaderboardEntryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data.DTO
+{
+    public class LeaderboardEntryDTO
+    {
+        public string PlayerFullName { get; set; }
+        public int Wins { get; set; }
+        public int TotalGamesPlayed { get; set; }
+        public double WinPercentage => TotalGamesPlayed > 0 ? (double)Wins / TotalGamesPlayed : 0;
+    }
+}
---
+++ TableTennis/ViewModels/LeaderboardEntryVM.cs
@@ -0,0 +1,11 @@
+﻿namespace TableTennis.ViewModels
+{
+    public class LeaderboardEntryVM
+    {
+        public int Rank { get; set; }
+        public string PlayerFullName { get; set; }
+        public int Wins { get; set; }
+        public int TotalGamesPlayed { get; set; }
+        public double WinPercentage { get; set; }
+    }
+}
